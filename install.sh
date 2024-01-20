097247cf3dbef603de75c61aea9633d6b88e53b6
Amadeusz Kosik <eternalsh@icloud.com>
2024-01-20 14:28:34 +0100

    Temporarily disable ZSH & dircolors config.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CWD="${0:a:h}"
 
-ln -sf "$CWD/dir_colors" 	"$HOME/.dir_colors"
-ln -sf "$CWD/zshrc" 	 	"$HOME/.zshrc"
+#ln -sf "$CWD/dir_colors" 	"$HOME/.dir_colors"
+#ln -sf "$CWD/zshrc" 	 	"$HOME/.zshrc"
 ln -sf "$CWD/vimrc"		"$HOME/.vimrc"
 
