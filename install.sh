54a6056fab5067cab3481f30293f079111cdb9f6
Amadeusz Kosik <eternalsh@protonmail.com>
2024-05-30 17:30:00 +0200

    Update and re-enable vim configuration file.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p "$HOME/.git/"
 
 #ln -sf "$CWD/dir_colors" 	       "$HOME/.dir_colors"
 #ln -sf "$CWD/zshrc" 	 	       "$HOME/.zshrc"
-#ln -sf "$CWD/vimrc"		       "$HOME/.vimrc"
+ln -sf "$CWD/vimrc"		          "$HOME/.vimrc"
 ln -sf "$CWD/git/commit-template" "$HOME/.git/commit-template"
 
 git config --global commit.template "$HOME/.git/commit-template"
