57c61934a4ab1b3adb3dd699165540c4c0dea902
Amadeusz Kosik <eternalsh@protonmail.com>
2024-05-30 17:25:10 +0200

    Add commit template for git.

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 CWD="${0:a:h}"
 
-#ln -sf "$CWD/dir_colors" 	"$HOME/.dir_colors"
-#ln -sf "$CWD/zshrc" 	 	"$HOME/.zshrc"
-ln -sf "$CWD/vimrc"		"$HOME/.vimrc"
+mkdir -p "$HOME/.git/"
 
+#ln -sf "$CWD/dir_colors" 	       "$HOME/.dir_colors"
+#ln -sf "$CWD/zshrc" 	 	       "$HOME/.zshrc"
+#ln -sf "$CWD/vimrc"		       "$HOME/.vimrc"
+ln -sf "$CWD/git/commit-template" "$HOME/.git/commit-template"
+
+git config --global commit.template "$HOME/.git/commit-template"
